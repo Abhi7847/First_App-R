ca04211bb4eb7d26cd7647f5d29ae1c3980144d5
ABHIRAM M <88245799+Abhi7847@users.noreply.github.com>
2021-09-15 14:25:27 +0530

    Update BMI_Calculator-Shiny.R

--- BMI_Calculator-Shiny.R
+++ BMI_Calculator-Shiny.R
@@ -3,14 +3,13 @@ ui <- shinyUI(fluidPage(
     mainPanel(
         numericInput("p0", "Enter Weight (in) Here:" ,0, min = 0, max = 200),
         numericInput("p1", "Enter Height (in meter) Here:", 0, min = 0, max = 3),
-        textOutput("a"))
+        textOutput("bmi"))
 ))
 server <- shinyServer(function(input, output,session){
-    output$a <- renderText({
+    output$bmi <- renderText({
         p0 <- input$p0
         p1 <- input$p1
-        alpha <- input$alpha
-        power = input$power
+
         paste("Your BMI is :", (p0/(p1*p1)) )
     })
 })
